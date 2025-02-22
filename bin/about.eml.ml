76720a95f7d7360515efb3a0632cdc91eb4172ad
Michael Dales <mwd24@cam.ac.uk>
2025-02-22 14:11:38 +0000

    Silly animation

--- bin/about.eml.ml
+++ bin/about.eml.ml
@@ -18,8 +18,14 @@ let render_page site sec _previous_page page _next_page =
                         <%s! Render.render_body page %>
                         </div>
                         <div id="mepic">
-                            <h3>&nbsp;</h3>
+                            <h2>&nbsp;</h2>
                             <img class="aboutme" src="<%s Section.url ~page sec %>thumbnail.jpg" srcset="<%s Section.url ~page sec %>thumbnail@2x.jpg 2x, <%s Section.url ~page sec %>thumbnail.jpg 1x"/>
+                            <div id="sidebar">
+                              <canvas
+                                id="side"
+                                width="400px"
+                                height="600px"/>
+                            </div>
                         </div>
                     </div>
                 </article>
@@ -32,5 +38,54 @@ let render_page site sec _previous_page page _next_page =
         <span>Digital Flapjack Ltd, UK Company 06788544</span>
       </div>
     </div>
+    <script>
+      let start;
+
+
+      function tick(timestamp) {
+        if (start === undefined) {
+          start = timestamp;
+        }
+        const t = timestamp - start;
+
+        const canvas = document.getElementById("side");
+        const ctx = canvas.getContext("2d");
+
+        const step = canvas.width / 10;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.strokeStyle = "rgb(127 180 0 / 50%)";
+
+        const z = 10 + (Math.sin(t / 100000) * 5);
+        const d = 10 + (Math.cos(t / 100000) * 5);
+
+        for (y = 0; y < (canvas.height / step); y ++) {
+          for (x = 0; x < (canvas.width / step); x ++) {
+
+            const c = (Math.sin(Math.sin((x + (t / 100)) / z)) +
+              Math.sin(Math.sin((y + (t/100)) / d))) * 5;
+            const r = (c + 8) / 3;
+
+            if (r > 0) {
+              ctx.beginPath();
+              ctx.lineWidth = (r / 2);
+              ctx.arc(
+                (x * step) + (step / 2),
+                (y * step) + (step / 2),
+                r, 0, Math.PI * 2, 0);
+              ctx.stroke();
+            }
+          }
+        }
+
+        window.requestAnimationFrame(tick);
+      };
+
+
+      document.addEventListener('DOMContentLoaded', function(){
+        window.requestAnimationFrame(tick);
+      });
+    </script>
   </body>
   </html>
